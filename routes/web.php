062dd6960a7292a163d7d4818b384e7171397961
Tubagus Kresna Bayu <tubaguskresnabayu@Administrators-MacBook-Air-2.local>
2025-01-14 13:48:56 +0700

    adjust path prototype to forsale

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => '/'], function () {
     Route::get('/', [HomeController::class, 'index']);
     Route::get('/cv', [HomeController::class, 'cv']);
-    Route::get('/prototype/{slug}', [HomeController::class, 'prototype']);
+    Route::get('/for-sale/{slug}', [HomeController::class, 'prototype']);
 
 });
